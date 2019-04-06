c69229306acce45cdb2725951e6d938be659ae2c
Matthew Pham <matthew.pham@cgs.act.edu.au>
2019-04-06 15:35:23 +1100

    Updated to localhost:5000 for testing

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ var express = require('express'),
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb', { useNewUrlParser: true });
-
+mongoose.connect('mongodb+srv://admin:Password1@cluster-default-tv3k8.mongodb.net/Tododb', { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
